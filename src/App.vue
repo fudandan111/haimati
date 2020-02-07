c5c4308c11f89ae29ce394cbf0cd894f2322ddb9
fudandan111 <1015339782@qq.com>
2020-02-07 22:37:44 +0800

    2.07---公共模块header和bottom

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 		<div class="header">
 			<img src="../public/img/logo.png" alt="">
 			<div id="nav">
-				<router-link to="/">首页</router-link> |
+				<router-link to="/" class="red">首页</router-link> |
 				<router-link to="/introduce">品牌介绍</router-link> |
 				<router-link to="/info">门店信息</router-link> |
 				<router-link to="/Cooperation">企业合作</router-link> |
@@ -11,10 +11,12 @@
 		</div>
 		<router-view/>
 		<div class="bottom">
-			<img src="../public/img/ico.png">
+			<div class="xiaoma">
+				<img src="../public/img/ico.png">
+			</div>
 			<div class="connect">
 				<ul>
-					<li>企业连接</li>
+					<li class="first">企业连接</li>
 					<li>缦图集团官网</li>
 					<li>海马体企业</li>
 					<li>西湖.缦图</li>
@@ -23,6 +25,7 @@
 			</div>
 			<div class="hotcity">
 				<ul>
+					<li class="first">热门城市</li>
 					<li>成都&nbsp;&nbsp;&nbsp;&nbsp;上海</li>
 					<li>北京&nbsp;&nbsp;&nbsp;&nbsp;武汉</li>
 					<li>重庆&nbsp;&nbsp;&nbsp;&nbsp;南京</li>
@@ -31,12 +34,23 @@
 			</div>
 			<div class="aboutHIMO">
 				<ul>
+					<li class="first">关于HIMO</li>
 					<li>常见问题</li>
 					<li>门店信息</li>
 					<li>企业合作</li>
 					<li>人才招聘</li>
 				</ul>
 			</div>
+			<div class="haimati">
+				<p class="first">联系海马体</p>
+				<p class="phone">400-880-1861</p>
+				<div class="imgs">
+					<img src="../public/img/img1.png" alt="">
+					<img src="../public/img/img2.png" alt="">
+					<img src="../public/img/img.png" alt="">
+				</div>
+			</div>
+			<div class="erweima"><img src="../public/img/erweima.png" alt=""></div>
 		</div>
 	</div>
 </template>
@@ -45,10 +59,6 @@
 	margin: 0;
 	padding: 0;
 	list-style: none;
-}
-html,
-body {
-	background-color: #efefef;
 }
 #app {
 	.header {
@@ -71,15 +81,72 @@ body {
 				font-size: 16px;
 				color: black;
 				text-decoration: none;
+				font-weight: bold;
+				&:hover {
+					color: #1769FF;
+				}
+			}
+			.red {
+				color: red;
 			}
 		}
 	}
 	.bottom {
 		border-top: solid 1px #E3E3E6;
-		background: #fff;
+		display: flex;
+		justify-content: space-between;
+		padding: 50px 150px 70px;
+		font-size: 12px;
+		color: #919199;
+		.first {
+			font-size: 14px;
+			font-weight: bold;
+			margin-bottom: 20px;
+			color: black;
+		}
+		li {
+			margin-bottom: 8px;
+		}
+		.xiaoma img {
+			width: 20px;
+			height: 50px;
+		}
+		.haimati {
+			.phone {
+				font-size: 20px;
+				color:#1769FF;
+				font-weight: bold;
+			}
+			.imgs {
+				img {
+					width: 30px;
+					height: 30px;
+					margin: 30px 3px 0;
+				}
+			}
+		}
+		.erweima img {
+			width: 120px;
+			height: 120px;
+		}
+
 	}
 }
 </style>
 <script>
+export default {
+	data() {
+		return {
 
+		}
+	},
+	watch: {
+		'$route': 'routerChage'
+	},
+	methods: {
+		routerChage(to) {
+			console.log(to.path)
+		}
+	}
+}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,12 @@
 	<div class="home">
 		<div class="banner">
 			<h1>首页</h1>
-			<div v-for="item in data.dataList" :key="item._id" :data="item"></div>
+			
 		</div>
 	</div>
 </template>
 <style lang="scss">
-.home {
-	background: #FAFAFA;
-}
+
 </style>
 <script>
 export default {
@@ -18,6 +16,7 @@ export default {
 		return {
 			data: {
 				dataList: ["../../public/img/banner1.jpg", "../../public/img/banner2.jpg"],
+				currentIndex: 0
 			}
 		}
 	},
